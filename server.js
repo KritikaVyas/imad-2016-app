c6fb5540bbe148e2f4e0a33c7db3774805f1488a
Kritika Vyas <kritichi9@gmail.com>
2016-09-29 15:16:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,18 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+
 app.get('/article-one', function (req, res){
-  res.sendFile(path.join(_dirname, 'ui', 'article-one.html'))  
+ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res){
-  res.sendFile(path.join(_dirname, 'ui', 'article-two.html'))  
+   res.send('Article two requested and will be send here'); 
 });
 
 app.get('/article-three', function (req, res){
-  res.sendFile(path.join(_dirname, 'ui', 'article-three.html'))  
+   res.send('Article three requested and will be send here'); 
 });
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
